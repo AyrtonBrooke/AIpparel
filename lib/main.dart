d3eedddd34e058ab294ea0d057f6d6624ccbd096
ayrton <ayrtonbrooke01@gmail.com>
2024-03-03 17:21:41 +0000

    flutter project

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:aiapparel/pages/outfits.dart';
+import 'package:aiapparel/pages/settings.dart';
+import 'package:aiapparel/pages/wardrobe.dart';
+import 'package:aiapparel/widgets/ThemeDemo/AppTheme.dart';
+import 'package:aiapparel/auth/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:aiapparel/firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'home/home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp( options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      home: const Login(),
+      debugShowCheckedModeBanner: false,
+    );
+
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: [
+          for (final tabItem in TabNavigationItem.items) tabItem.page,
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.lightBlueAccent,
+        selectedItemColor: Colors.greenAccent,
+        unselectedItemColor: Colors.black45,
+        onTap: (int index) => setState(() => _currentIndex = index),
+        items: [
+          for (final tabItem in TabNavigationItem.items)
+            BottomNavigationBarItem(
+              icon: tabItem.icon,
+              label: tabItem.title,
+            )
+        ],
+      ),
+    );
+  }
+}
+
+class TabNavigationItem {
+  final Widget page;
+  final String title;
+  final Icon icon;
+
+  TabNavigationItem({
+    required this.page,
+    required this.title,
+    required this.icon,
+  });
+
+  static List<TabNavigationItem> get items => [
+    TabNavigationItem(
+      page: const HomePage(),
+      icon: const Icon(Icons.home),
+      title: "Home",
+    ),
+    TabNavigationItem(
+      page: const OutfitsPage(),
+      icon: const Icon(Icons.checkroom),
+      title: "Outfits",
+    ),
+    TabNavigationItem(
+      page: const WardrobePage(),
+      icon: const Icon(Icons.door_sliding),
+      title: "Wardrobe",
+    ),
+    TabNavigationItem(
+      page: const SettingsPage(),
+      icon: const Icon(Icons.settings),
+      title: "Settings",
+    ),
+  ];
+}
